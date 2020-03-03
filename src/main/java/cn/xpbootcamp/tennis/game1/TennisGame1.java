a5dee0f7e3e296334e1e55f797ef0f6955649e01
Lei Zhu <lei.zhu1@thoughtworks.com>
2020-03-03 22:04:20 +0800

    refactor:Delete unused method and Rename fields

--- src/main/java/cn/xpbootcamp/tennis/game1/TennisGame1.java
+++ src/main/java/cn/xpbootcamp/tennis/game1/TennisGame1.java
@@ -4,28 +4,24 @@ import cn.xpbootcamp.tennis.TennisGame;
 
 public class TennisGame1 implements TennisGame {
 
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+    private int mScore1 = 0;
+    private int mScore2 = 0;
 
     public TennisGame1(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
+        if (playerName.equals("player1"))
+            mScore1 += 1;
         else
-            m_score2 += 1;
+            mScore2 += 1;
     }
 
     public String getScore() {
         String score = "";
         int tempScore = 0;
-        if (m_score1 == m_score2) {
-            switch (m_score1) {
+        if (mScore1 == mScore2) {
+            switch (mScore1) {
                 case 0:
                     score = "Love-All";
                     break;
@@ -40,18 +36,18 @@ public class TennisGame1 implements TennisGame {
                     break;
 
             }
-        } else if (m_score1 >= 4 || m_score2 >= 4) {
-            int minusResult = m_score1 - m_score2;
+        } else if (mScore1 >= 4 || mScore2 >= 4) {
+            int minusResult = mScore1 - mScore2;
             if (minusResult == 1) score = "Advantage player1";
             else if (minusResult == -1) score = "Advantage player2";
             else if (minusResult >= 2) score = "Win for player1";
             else score = "Win for player2";
         } else {
             for (int i = 1; i < 3; i++) {
-                if (i == 1) tempScore = m_score1;
+                if (i == 1) tempScore = mScore1;
                 else {
                     score += "-";
-                    tempScore = m_score2;
+                    tempScore = mScore2;
                 }
                 switch (tempScore) {
                     case 0:
