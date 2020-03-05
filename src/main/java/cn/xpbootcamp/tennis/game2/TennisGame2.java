e3ca617c240b4f83f74b98b784a9420d1e890d23
Lei Zhu <lei.zhu1@thoughtworks.com>
2020-03-05 22:48:16 +0800

    refactor:reduce judgement branch

--- src/main/java/cn/xpbootcamp/tennis/game2/TennisGame2.java
+++ src/main/java/cn/xpbootcamp/tennis/game2/TennisGame2.java
@@ -3,6 +3,7 @@ package cn.xpbootcamp.tennis.game2;
 import cn.xpbootcamp.tennis.TennisGame;
 
 import static cn.xpbootcamp.tennis.PlayerConstants.PLAYER_1_NAME;
+import static java.lang.String.*;
 
 public class TennisGame2 implements TennisGame {
     public int p1Point = 0;
@@ -17,65 +18,32 @@ public class TennisGame2 implements TennisGame {
     public String getScore() {
         String score = "";
         if (p1Point == p2Point && p1Point < 4) {
-            if (p1Point == 0)
-                score = "Love";
-            if (p1Point == 1)
-                score = "Fifteen";
-            if (p1Point == 2)
-                score = "Thirty";
-            score += "-All";
+            score = format("%s-All", getScoreNameByNum(p1Point));
         }
         if (p1Point == p2Point && p1Point >= 3)
             score = "Deuce";
 
         if (p1Point > 0 && p2Point == 0) {
-            String score1;
-            if (p1Point == 1)
-                p1Result = "Fifteen";
-            if (p1Point == 2)
-                p1Result = "Thirty";
-            if (p1Point == 3)
-                p1Result = "Forty";
-
+            p1Result = getScoreNameByNum(p1Point);
             p2Result = "Love";
-            score1 = p1Result + "-" + p2Result;
-            score = score1;
+            score = format("%s-%s", p1Result, p2Result);
         }
         if (p2Point > 0 && p1Point == 0) {
-            String score1;
-            if (p2Point == 1)
-                p2Result = "Fifteen";
-            if (p2Point == 2)
-                p2Result = "Thirty";
-            if (p2Point == 3)
-                p2Result = "Forty";
-
+            p2Result = getScoreNameByNum(p2Point);
             p1Result = "Love";
-            score1 = p1Result + "-" + p2Result;
-            score = score1;
+            score = format("%s-%s", p1Result, p2Result);
         }
 
         if (p1Point > p2Point && p1Point < 4) {
-            if (p1Point == 2)
-                p1Result = "Thirty";
-            if (p1Point == 3)
-                p1Result = "Forty";
-            if (p2Point == 1)
-                p2Result = "Fifteen";
-            if (p2Point == 2)
-                p2Result = "Thirty";
-            score = p1Result + "-" + p2Result;
+            p1Result = getScoreNameByNum(p1Point);
+            p2Result = getScoreNameByNum(p2Point);
+            score = format("%s-%s", p1Result, p2Result);
         }
         if (p2Point > p1Point && p2Point < 4) {
-            if (p2Point == 2)
-                p2Result = "Thirty";
-            if (p2Point == 3)
-                p2Result = "Forty";
-            if (p1Point == 1)
-                p1Result = "Fifteen";
-            if (p1Point == 2)
-                p1Result = "Thirty";
-            score = p1Result + "-" + p2Result;
+
+            p2Result = getScoreNameByNum(p2Point);
+            p1Result = getScoreNameByNum(p1Point);
+            score = format("%s-%s", p1Result, p2Result);
         }
 
         if (p1Point > p2Point && p2Point >= 3) {
@@ -93,6 +61,20 @@ public class TennisGame2 implements TennisGame {
             score = "Win for player2";
         }
         return score;
+    }
+
+    private String getScoreNameByNum(int point) {
+        switch (point) {
+            case 0:
+                return "Love";
+            case 1:
+                return "Fifteen";
+            case 2:
+                return "Thirty";
+            case 3:
+                return "Forty";
+        }
+        return "";
     }
 
     public void wonPoint(String player) {
