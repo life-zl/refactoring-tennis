27e5b673802d8d63420d833ec95b2fcd6d4f027d
Lei Zhu <lei.zhu1@thoughtworks.com>
2020-03-07 14:57:02 +0800

    remove unused method

--- src/main/java/cn/xpbootcamp/tennis/game1/TennisGameImpl.java
+++ src/main/java/cn/xpbootcamp/tennis/game1/TennisGameImpl.java
@@ -2,14 +2,12 @@ package cn.xpbootcamp.tennis.game1;
 
 import cn.xpbootcamp.tennis.TennisGame;
 
-import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class TennisGameImpl implements TennisGame {
 
     private Player player1;
     private Player player2;
-
     public TennisGameImpl(String player1Name, String player2Name) {
         this.player1 = new Player(player1Name);
         this.player2 = new Player(player2Name);
