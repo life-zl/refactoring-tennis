a8dce4a72b437cbf00996accd78204cee3cc9d46
Lei Zhu <lei.zhu1@thoughtworks.com>
2020-03-07 11:51:47 +0800

    refactor ut

--- src/test/java/cn/xpbootcamp/tennis/TennisGameTest.java
+++ src/test/java/cn/xpbootcamp/tennis/TennisGameTest.java
@@ -5,80 +5,88 @@ import cn.xpbootcamp.tennis.game1.TennisGame1;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.stream.Stream;
 
 import static org.junit.Assert.assertEquals;
 
 class TennisGameTest {
-    private int player1Score;
-    private int player2Score;
-    private String expectedScore;
+    public static final String PLAYER_1_NAME = "player1";
+    public static final String PLAYER_2_NAME = "player2";
 
-    public static Stream<List> getAllScores() {
+
+    static class ScoreItem{
+        int player1score;
+        int player2Score;
+        String expectedResult;
+
+        private ScoreItem(int player1score, int player2Score, String expectedResult) {
+            this.player1score = player1score;
+            this.player2Score = player2Score;
+            this.expectedResult = expectedResult;
+        }
+
+        public static ScoreItem create(int score1, int score2, String result) {
+            return new ScoreItem(score1, score2, result);
+        }
+
+
+    }
+
+    public static Stream<ScoreItem> getAllScores() {
         return Stream.of(
-                Arrays.asList(0, 0, "Love-All"),
-                Arrays.asList(1, 1, "Fifteen-All"),
-                Arrays.asList(2, 2, "Thirty-All"),
-                Arrays.asList(3, 3, "Deuce"),
-                Arrays.asList(4, 4, "Deuce"),
+                ScoreItem.create(0, 0, "Love-All"),
+                ScoreItem.create(1, 1, "Fifteen-All"),
+                ScoreItem.create(2, 2, "Thirty-All"),
+                ScoreItem.create(3, 3, "Deuce"),
+                ScoreItem.create(4, 4, "Deuce"),
 
-                Arrays.asList(1, 0, "Fifteen-Love"),
-                Arrays.asList(0, 1, "Love-Fifteen"),
-                Arrays.asList(2, 0, "Thirty-Love"),
-                Arrays.asList(0, 2, "Love-Thirty"),
-                Arrays.asList(3, 0, "Forty-Love"),
-                Arrays.asList(0, 3, "Love-Forty"),
-                Arrays.asList(4, 0, "Win for player1"),
-                Arrays.asList(0, 4, "Win for player2"),
+                ScoreItem.create(1, 0, "Fifteen-Love"),
+                ScoreItem.create(0, 1, "Love-Fifteen"),
+                ScoreItem.create(2, 0, "Thirty-Love"),
+                ScoreItem.create(0, 2, "Love-Thirty"),
+                ScoreItem.create(3, 0, "Forty-Love"),
+                ScoreItem.create(0, 3, "Love-Forty"),
+                ScoreItem.create(4, 0, "Win for player1"),
+                ScoreItem.create(0, 4, "Win for player2"),
 
-                Arrays.asList(2, 1, "Thirty-Fifteen"),
-                Arrays.asList(1, 2, "Fifteen-Thirty"),
-                Arrays.asList(3, 1, "Forty-Fifteen"),
-                Arrays.asList(1, 3, "Fifteen-Forty"),
-                Arrays.asList(4, 1, "Win for player1"),
-                Arrays.asList(1, 4, "Win for player2"),
+                ScoreItem.create(2, 1, "Thirty-Fifteen"),
+                ScoreItem.create(1, 2, "Fifteen-Thirty"),
+                ScoreItem.create(3, 1, "Forty-Fifteen"),
+                ScoreItem.create(1, 3, "Fifteen-Forty"),
+                ScoreItem.create(4, 1, "Win for player1"),
+                ScoreItem.create(1, 4, "Win for player2"),
 
-                Arrays.asList(3, 2, "Forty-Thirty"),
-                Arrays.asList(2, 3, "Thirty-Forty"),
-                Arrays.asList(4, 2, "Win for player1"),
-                Arrays.asList(2, 4, "Win for player2"),
+                ScoreItem.create(3, 2, "Forty-Thirty"),
+                ScoreItem.create(2, 3, "Thirty-Forty"),
+                ScoreItem.create(4, 2, "Win for player1"),
+                ScoreItem.create(2, 4, "Win for player2"),
 
-                Arrays.asList(4, 3, "Advantage player1"),
-                Arrays.asList(3, 4, "Advantage player2"),
-                Arrays.asList(5, 4, "Advantage player1"),
-                Arrays.asList(4, 5, "Advantage player2"),
-                Arrays.asList(15, 14, "Advantage player1"),
-                Arrays.asList(14, 15, "Advantage player2"),
+                ScoreItem.create(4, 3, "Advantage player1"),
+                ScoreItem.create(3, 4, "Advantage player2"),
+                ScoreItem.create(5, 4, "Advantage player1"),
+                ScoreItem.create(4, 5, "Advantage player2"),
+                ScoreItem.create(15, 14, "Advantage player1"),
+                ScoreItem.create(14, 15, "Advantage player2"),
 
-                Arrays.asList(6, 4, "Win for player1"),
-                Arrays.asList(4, 6, "Win for player2"),
-                Arrays.asList(16, 14, "Win for player1"),
-                Arrays.asList(14, 16, "Win for player2"));
+                ScoreItem.create(6, 4, "Win for player1"),
+                ScoreItem.create(4, 6, "Win for player2"),
+                ScoreItem.create(16, 14, "Win for player1"),
+                ScoreItem.create(14, 16, "Win for player2"));
     }
 
     @ParameterizedTest
     @MethodSource("getAllScores")
-    public void checkAllScoresTennisGame1(List<Object> params) {
-        TennisGame1 game = new TennisGame1("player1", "player2");
-        checkAllScores(params, game);
-    }
-
-
-    public void checkAllScores(List<Object> params, TennisGame game) {
-        player1Score = (int) params.get(0);
-        player2Score = (int) params.get(1);
-        expectedScore = (String) params.get(2);
-
-        int highestScore = Math.max(this.player1Score, this.player2Score);
+    public void checkAllScoresTennisGame1(ScoreItem scoreItem) {
+        TennisGame1 game = new TennisGame1(PLAYER_1_NAME, PLAYER_2_NAME);
+        int highestScore = Math.max(scoreItem.player1score, scoreItem.player2Score);
         for (int i = 0; i < highestScore; i++) {
-            if (i < this.player1Score)
-                game.wonPoint("player1");
-            if (i < this.player2Score)
-                game.wonPoint("player2");
+            if (i < scoreItem.player1score)
+                ((TennisGame) game).wonPoint(PLAYER_1_NAME);
+            if (i < scoreItem.player2Score)
+                ((TennisGame) game).wonPoint(PLAYER_2_NAME);
         }
-        assertEquals(this.expectedScore, game.getScore());
+        assertEquals(scoreItem.expectedResult, ((TennisGame) game).getScore());
     }
+
 
 }
