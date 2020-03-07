c0187dcfe77bcbfa87dad9c5abb1b9bcef62c61e
Lei Zhu <lei.zhu1@thoughtworks.com>
2020-03-07 14:04:30 +0800

    clean up simply

--- src/test/java/cn/xpbootcamp/tennis/TennisGameTest.java
+++ src/test/java/cn/xpbootcamp/tennis/TennisGameTest.java
@@ -1,9 +1,7 @@
 package cn.xpbootcamp.tennis;
 
 
-import cn.xpbootcamp.tennis.game1.TennisGame1;
-import cn.xpbootcamp.tennis.game2.TennisGame2;
-import cn.xpbootcamp.tennis.game3.TennisGame3;
+import cn.xpbootcamp.tennis.game1.TennisGameImpl;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
@@ -63,21 +61,7 @@ class TennisGameTest {
     @ParameterizedTest
     @MethodSource("getAllScores")
     public void checkAllScoresTennisGame1(List<Object> params) {
-        TennisGame1 game = new TennisGame1("player1", "player2");
-        checkAllScores(params, game);
-    }
-
-    @ParameterizedTest
-    @MethodSource("getAllScores")
-    public void checkAllScoresTennisGame2(List<Object> params) {
-        TennisGame2 game = new TennisGame2("player1", "player2");
-        checkAllScores(params, game);
-    }
-
-    @ParameterizedTest
-    @MethodSource("getAllScores")
-    public void checkAllScoresTennisGame3(List<Object> params) {
-        TennisGame3 game = new TennisGame3("player1", "player2");
+        TennisGameImpl game = new TennisGameImpl("player1", "player2");
         checkAllScores(params, game);
     }
 
