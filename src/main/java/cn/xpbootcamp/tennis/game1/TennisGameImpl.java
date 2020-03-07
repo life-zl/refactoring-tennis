282d04066e940882f9c5d9639119c39e92210b58
Lei Zhu <lei.zhu1@thoughtworks.com>
2020-03-07 14:33:42 +0800

    add player1 and player2

--- src/main/java/cn/xpbootcamp/tennis/game1/TennisGameImpl.java
+++ src/main/java/cn/xpbootcamp/tennis/game1/TennisGameImpl.java
@@ -4,66 +4,63 @@ import cn.xpbootcamp.tennis.TennisGame;
 
 public class TennisGameImpl implements TennisGame {
 
-    private int player1Score = 0;
-    private int player2Score = 0;
-    private String player1Name;
-    private String player2Name;
+    private Player player1;
+    private Player player2;
 
     public TennisGameImpl(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
+        this.player1 = new Player(player1Name);
+        this.player2 = new Player(player2Name);
     }
 
     public void wonPoint(String playerName) {
-        if (playerName.equals(player1Name))
-            player1Score += 1;
+        if (playerName.equals(player1.getName()))
+            player1.increaseOne();
         else
-            player2Score += 1;
+            player2.increaseOne();
     }
 
     public String getScore() {
-
         String[] scores = {"Love", "Fifteen", "Thirty", "Forty"};
-        if (isTieScore(player1Score, player2Score)) {
+        if (isTieScore(player1, player2)) {
             return getTieScore(scores);
         }
-        if (isAdvantage(player1Score, player2Score)) {
+        if (isAdvantage(player1, player2)) {
             return "Advantage player1";
         }
-        if (isAdvantage(player2Score, player1Score)) {
+        if (isAdvantage(player2, player1)) {
             return "Advantage player2";
         }
-        if (isWin(player1Score, player2Score)) {
+        if (isWin(player1, player2)) {
             return "Win for player1";
         }
-        if (isWin(player2Score, player1Score)) {
+        if (isWin(player2, player1)) {
             return "Win for player2";
         }
         return getCommonScore(scores);
     }
 
-    private boolean isWin(int player1Score, int player2Score) {
-        return isGreaterThan4Score() && player1Score - player2Score >= 2;
+    private boolean isWin(Player player1, Player player2) {
+        return isGreaterThan4Score() && player1.getScore() - player2.getScore() >= 2;
     }
 
-    private boolean isAdvantage(int player1Score, int player2Score) {
-        return isGreaterThan4Score() && player1Score - player2Score == 1;
+    private boolean isAdvantage(Player player1, Player player2) {
+        return isGreaterThan4Score() && player1.getScore() - player2.getScore() == 1;
     }
 
     private String getTieScore(String[] scores) {
-        return player1Score > 2 ? "Deuce" : scores[player1Score] + "-All";
+        return player1.getScore() > 2 ? "Deuce" : scores[player1.getScore()] + "-All";
     }
 
     private String getCommonScore(String[] scores) {
-        return scores[player1Score] + "-" + scores[player2Score];
+        return scores[player1.getScore()] + "-" + scores[player2.getScore()];
     }
 
     private boolean isGreaterThan4Score() {
-        return player1Score >= 4 || player2Score >= 4;
+        return player1.getScore() >= 4 || player2.getScore() >= 4;
     }
 
-    private boolean isTieScore(int player1Score, int player2Score) {
-        return player1Score == player2Score;
+    private boolean isTieScore(Player player1, Player player2) {
+        return player1.getScore() == player2.getScore();
     }
 
 }
